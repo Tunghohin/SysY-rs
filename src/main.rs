cbb4dd8ce2e6ce5d64289370ae8e3018d47789e4
Tunghohin <houhin0809@gmail.com>
2025-08-26 01:11:35 +0800

    try lab1

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{
+    env,
+    fs,
+};
+
+mod lexer;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+    let input = fs::read_to_string(&args[1])
+        .expect("Failed to read file");
+
+    lexer::tokenize(&input)
+        .unwrap_or_else(|e| {
+            eprintln!("{}", e);
+            vec![]
+        })
+        .iter()
+        .for_each(|token| match token.kind {
+            lexer::token::TokenKind::Eof => {}
+            _ => {
+                eprintln!("{}", token);
+            }
+        })
+}
