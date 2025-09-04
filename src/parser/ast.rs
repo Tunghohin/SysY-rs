bcf9282a0347212dfb439e5d097f010d516ad24e
Tunghohin <houhin0809@gmail.com>
2025-09-04 17:47:33 +0800

    expand AST node definitions and improve AST display functionality

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,8 +1,190 @@
+#![allow(dead_code)]
+
+use std::ops::Add;
+
 #[derive(Debug)]
 pub enum AstNode {
     CompUnit(Vec<Box<AstNode>>),
+
+    Decl(Box<AstNode>),
+
+    BType(String),
+
     ConstDecl {
         btype: Box<AstNode>,
         const_defs: Vec<Box<AstNode>>,
     },
+    ConstDef {
+        ident: String,
+        dimensions: Vec<Box<AstNode>>,
+        init_val: Box<AstNode>,
+    },
+    ConstInitVal(ConstInitValType),
+
+    VarDecl {
+        btype: Box<AstNode>,
+        var_defs: Vec<Box<AstNode>>,
+    },
+    VarDef {
+        ident: String,
+        dimensions: Vec<Box<AstNode>>,
+        init_val: Option<Box<AstNode>>,
+    },
+    InitVal(InitValType),
+
+    FuncDef {
+        func_type: Box<AstNode>,
+        ident: String,
+        params: Option<Box<AstNode>>,
+        body: Box<AstNode>,
+    },
+    FuncType(String),
+    FuncFParams(Vec<Box<AstNode>>),
+    FuncFParam {
+        btype: Box<AstNode>,
+        ident: String,
+        is_array: bool,
+        dimensions: Vec<Box<AstNode>>,
+    },
+
+    Block(Vec<Box<AstNode>>),
+    BlockItem(Box<AstNode>),
+
+    Stmt(Box<StmtType>),
+
+    Exp(Box<AstNode>),
+    Cond(Box<AstNode>),
+    LVal {
+        ident: String,
+        dimensions: Vec<Box<AstNode>>,
+    },
+    PrimaryExp(PrimaryExpType),
+    Number(NumberType),
+    UnaryExp(UnaryExpType),
+    UnaryOp(UnaryOpType),
+    FuncRParams(Vec<Box<AstNode>>),
+    MulExp {
+        lhs: Box<AstNode>,
+        ops: Vec<(MulOpType, Box<AstNode>)>,
+    },
+    AddExp {
+        lhs: Box<AstNode>,
+        ops: Vec<(AddOpType, Box<AstNode>)>,
+    },
+    RelExp {
+        lhs: Box<AstNode>,
+        ops: Vec<(RelOpType, Box<AstNode>)>,
+    },
+    EqExp {
+        lhs: Box<AstNode>,
+        ops: Vec<(EqOpType, Box<AstNode>)>,
+    },
+    LAndExp {
+        lhs: Box<AstNode>,
+        ops: Vec<(LogicOpType, Box<AstNode>)>,
+    },
+    LOrExp {
+        lhs: Box<AstNode>,
+        ops: Vec<(LogicOpType, Box<AstNode>)>,
+    },
+    ConstExp(Box<AstNode>),
+}
+
+#[derive(Debug)]
+pub enum UnaryExpType {
+    FuncCall {
+        ident: String,
+        args: Option<Vec<Box<AstNode>>>,
+    },
+    PrimaryExp(Box<AstNode>),
+    Unary {
+        op: Box<AstNode>,
+        exp: Box<AstNode>,
+    },
+}
+
+#[derive(Debug)]
+pub enum UnaryOpType {
+    Plus,
+    Minus,
+    Not,
+}
+
+#[derive(Debug)]
+pub enum MulOpType {
+    Mul,
+    Div,
+    Mod,
+}
+
+#[derive(Debug)]
+pub enum AddOpType {
+    Plus,
+    Minus,
+}
+
+#[derive(Debug)]
+pub enum RelOpType {
+    Lt,
+    Gt,
+    Le,
+    Ge,
+}
+
+#[derive(Debug)]
+pub enum EqOpType {
+    Eq,
+    Neq,
+}
+
+#[derive(Debug)]
+pub enum LogicOpType {
+    And,
+    Or,
+}
+
+#[derive(Debug)]
+pub enum PrimaryExpType {
+    Exp(Box<AstNode>),
+    LVal(Box<AstNode>),
+    Number(Box<AstNode>),
+}
+
+#[derive(Debug)]
+pub enum StmtType {
+    Assign {
+        lval: Box<AstNode>,
+        exp: Box<AstNode>,
+    },
+    Exp(Option<Box<AstNode>>),
+    Block(Box<AstNode>),
+    If {
+        cond: Box<AstNode>,
+        then_stmt: Box<AstNode>,
+        else_stmt: Option<Box<AstNode>>,
+    },
+    While {
+        cond: Box<AstNode>,
+        stmt: Box<AstNode>,
+    },
+    Break,
+    Continue,
+    Return(Option<Box<AstNode>>),
+}
+
+#[derive(Debug)]
+pub enum ConstInitValType {
+    Exp(Box<AstNode>),
+    List(Vec<Box<AstNode>>),
+}
+
+#[derive(Debug)]
+pub enum InitValType {
+    Exp(Box<AstNode>),
+    List(Vec<Box<AstNode>>),
+}
+
+#[derive(Debug)]
+pub enum NumberType {
+    IntegerConst(i32),
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 mod ast;
 
 use ast::AstNode;
@@ -8,32 +10,56 @@ use pest_derive::Parser;
 #[grammar = "./rules.pest"]
 pub(crate) struct SysYParser;
 
-pub struct ASTBuilder {
-}
+pub struct ASTBuilder {}
 
 impl ASTBuilder {
-    fn build_ast(pair: pest::iterators::Pair<Rule>, depth: usize) {
+    fn build_ast(pair: pest::iterators::Pair<Rule>) -> Result<Box<AstNode>, String> {
+        match pair.as_rule() {
+            Rule::CompUnit => {
+                let children = pair
+                    .into_inner()
+                    .map(ASTBuilder::build_ast)
+                    .collect::<Result<Vec<_>, _>>()?;
+                Ok(Box::new(AstNode::CompUnit(children)))
+            }
+
+            _ => unimplemented!(),
+        }
     }
 
-    fn display_ast(pair: pest::iterators::Pair<Rule>, depth: usize) {
-        let indent = "  ".repeat(depth);
-        let rule_type = pair.as_rule();
-        println!("{}Rule: {:?}", indent, rule_type);
- 
-        pair.into_inner().for_each(|inner| {
-            ASTBuilder::display_ast(inner, depth + 1);
-        });
+    fn display_ast_inner(pair: pest::iterators::Pair<Rule>, prefix: String, is_last: bool) {
+        let connector = if is_last { "└── " } else { "├── " };
+        println!("{}{}{:?}", prefix, connector, pair.as_rule());
+
+        let mut inner = pair.into_inner().peekable();
+        while let Some(p) = inner.next() {
+            let is_last_child = inner.peek().is_none();
+            let new_prefix = if is_last {
+                format!("{}    ", prefix)
+            } else {
+                format!("{}│   ", prefix)
+            };
+            ASTBuilder::display_ast_inner(p, new_prefix, is_last_child);
+        }
+    }
+
+    fn display_ast(pair: pest::iterators::Pair<Rule>) {
+        println!("AST");
+        ASTBuilder::display_ast_inner(pair, "".to_string(), true);
     }
 }
 
 pub fn parse(input: &str) -> Result<(), String> {
     let parse_result = SysYParser::parse(Rule::parse, input).map_err(|e| format!("{}", e))?;
-    ASTBuilder::display_ast(parse_result.into_iter().next().unwrap(), 0);
+    ASTBuilder::display_ast(parse_result.into_iter().next().unwrap());
     Ok(())
 }
 
 #[test]
 fn test_parser() {
     let src = std::fs::read_to_string("./tests/parser/sample2.in").unwrap_or_default();
-    parse(&src).unwrap_or_else(|e| { println!("{}", e); panic!() });
+    parse(&src).unwrap_or_else(|e| {
+        println!("{}", e);
+        panic!()
+    });
 }
