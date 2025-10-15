ecb79d84d2c54200a8526385932348fafda45bd1
Tunghohin <houhin0809@gmail.com>
2025-10-15 17:28:48 +0800

    update: support type lift

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -947,7 +947,24 @@ impl<'ctx> Codegen<'ctx> {
                 for (op, rhs_node) in ops {
                     let rhs = self.gen_exp(rhs_node)?;
                     let lhs_val = self.into_int_value_helper(left)?;
-                    let rhs_val = self.into_int_value_helper(rhs)?;
+                    let mut rhs_val = self.into_int_value_helper(rhs)?;
+
+                    if lhs_val.get_type().get_bit_width() == 32
+                        && rhs_val.get_type().get_bit_width() == 1
+                    {
+                        rhs_val = self
+                            .builder
+                            .build_int_z_extend(rhs_val, self.ctx.i32_type(), "zext")
+                            .map_err(|e| e.to_string())?;
+                    } else if lhs_val.get_type().get_bit_width() == 1
+                        && rhs_val.get_type().get_bit_width() == 32
+                    {
+                        rhs_val = self
+                            .builder
+                            .build_int_truncate(rhs_val, self.ctx.bool_type(), "trunc")
+                            .map_err(|e| e.to_string())?;
+                    }
+
                     left = match op {
                         MulOpInner::Mul => self
                             .builder
@@ -973,7 +990,24 @@ impl<'ctx> Codegen<'ctx> {
                 for (op, rhs_node) in ops {
                     let rhs = self.gen_exp(rhs_node)?;
                     let lhs_val = self.into_int_value_helper(left)?;
-                    let rhs_val = self.into_int_value_helper(rhs)?;
+                    let mut rhs_val = self.into_int_value_helper(rhs)?;
+
+                    if lhs_val.get_type().get_bit_width() == 32
+                        && rhs_val.get_type().get_bit_width() == 1
+                    {
+                        rhs_val = self
+                            .builder
+                            .build_int_z_extend(rhs_val, self.ctx.i32_type(), "zext")
+                            .map_err(|e| e.to_string())?;
+                    } else if lhs_val.get_type().get_bit_width() == 1
+                        && rhs_val.get_type().get_bit_width() == 32
+                    {
+                        rhs_val = self
+                            .builder
+                            .build_int_truncate(rhs_val, self.ctx.bool_type(), "trunc")
+                            .map_err(|e| e.to_string())?;
+                    }
+
                     left = match op {
                         AddOpInner::Plus => self
                             .builder
@@ -1358,7 +1392,7 @@ fn codegen_dummy() {
 
 #[test]
 fn codegen() {
-    let src = std::fs::read_to_string("tests/codegen/sample6.in").unwrap_or_default();
+    let src = std::fs::read_to_string("tests/codegen/hack5.in").unwrap_or_default();
     let ast = parse(&src, BuildConfig::default())
         .map_err(|e| println!("{}", e))
         .unwrap_or_else(|_| panic!("Failed to parse source code"));
