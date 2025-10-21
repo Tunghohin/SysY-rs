fd6b61a33f9cddf74bdc207148830c39b974fcec
Tunghohin <houhin0809@gmail.com>
2025-10-21 16:22:22 +0800

    update: modify argument count for usage and write assembly output to specified file

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <filename>", args[0]);
+    if args.len() < 3 {
+        eprintln!("Usage: {} <filename> <filename>", args[0]);
         std::process::exit(1);
     }
     let input = fs::read_to_string(&args[1]).expect("Failed to read file");
@@ -37,5 +37,6 @@ fn main() {
     asm_gen
         .gen_asm()
         .unwrap_or_else(|e| panic!("Failed to generate assembly: {}", e));
-    println!("{}", asm_gen.emit());
+
+    fs::write(&args[2], asm_gen.emit()).expect("Unable to write file");
 }
